4de20ee77258083d46fe614507ae40a39b06c014
WoollyOne <hello@woolly.one>
2022-01-27 20:03:39 -0700

    Separate webpack into two different files

---
+++ client.webpack.config.js
@@ -0,0 +1,38 @@
+const nodeExternals = require('webpack-node-externals');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        'public/client': './src/ts/index.ts',
+    },
+    target: 'web',
+    module: {
+        rules: [
+            {
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }, {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    externals: [nodeExternals()],
+    output: {
+        filename: '[name].js',
+        chunkFilename: '[name].js',
+    },
+    plugins: [
+            new CopyWebpackPlugin({ patterns: [
+                { from: 'src/html/index.html', to: 'public/index.html' },
+                { from: 'src/html/index.css', to: 'public/index.css' },
+                { from: 'src/wav/*.wav', to: './public/sound/[name].wav' },
+            ] 
+        })
+    ]
+};
